792a8ff2871899532274e338ff1c0c9be4945135
vodoo <VodooPeople@users.noreply.github.com>
2017-04-13 08:14:51 -0600

    files basics

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent }  from './app.component';
+import { headerComponent } from './header/app.headerComponent';
+import { navComponent } from './nav/app.navComponent';
+import { contentAreaComponent } from './contentArea/app.contentAreaComponent';
+import { footerComponent } from './footer/app.footerComponent';
+import { aboutComponent } from './about/app.aboutComponent';
+
+@NgModule({
+  imports:      [ BrowserModule, 
+  					RouterModule.forRoot([
+  							{
+  								path: '', 
+  								component: contentAreaComponent
+  							}, 
+  							{ 
+  								path: 'about', 
+  								component: aboutComponent
+  							}
+  						])
+
+
+
+   ],
+  declarations: [ 	AppComponent, 
+  					headerComponent, 
+  					navComponent, 
+  					contentAreaComponent, 
+  					footerComponent, 
+  					aboutComponent
+  					],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
